340412f815e7182e423d5f91ad7157ca43755bc0
Tom Hayosh <thayosh@gmail.com>
2018-12-29 09:13:52 -0600

    Revert "Eliminated unnecessary temp variables from views"
    This reverts commit 8ed1b2b66699dee9dc8a81f412149cf3275b07a9.

--- pool/views.py
+++ pool/views.py
@@ -18,20 +18,31 @@ def view_picks(request, pick_set_id):
 
 
 def new_picks(request):
+    # Selecting by [0] here gives the first char of '24' from ('24',)
+    temp_round_1_game_1 = request.POST['game_1_pick'],
+    temp_round_1_game_2 = request.POST['game_2_pick'],
+    temp_round_1_game_3 = request.POST['game_3_pick'],
+    temp_round_1_game_4 = request.POST['game_4_pick'],
     pick_set = PickSet.objects.create(
-        round_1_game_1=request.POST['game_1_pick'],
-        round_1_game_2=request.POST['game_2_pick'],
-        round_1_game_3=request.POST['game_3_pick'],
-        round_1_game_4=request.POST['game_4_pick'],
+        # Selecting by [0] here gives the integer 24. Weird.
+        round_1_game_1=temp_round_1_game_1[0],
+        round_1_game_2=temp_round_1_game_2[0],
+        round_1_game_3=temp_round_1_game_3[0],
+        round_1_game_4=temp_round_1_game_4[0],
     )
     return redirect(f'/picks/{pick_set.id}/')
 
 
 def update_picks(request, pick_set_id):
     pick_set = PickSet.objects.get(id=pick_set_id)
-    pick_set.round_1_game_1 = request.POST['game_1_pick']
-    pick_set.round_1_game_2 = request.POST['game_2_pick']
-    pick_set.round_1_game_3 = request.POST['game_3_pick']
-    pick_set.round_1_game_4 = request.POST['game_4_pick']
+    # Selecting by [0] here gives the first char of '24' from ('24',)
+    temp_round_1_game_1 = request.POST['game_1_pick'],
+    temp_round_1_game_2 = request.POST['game_2_pick'],
+    temp_round_1_game_3 = request.POST['game_3_pick'],
+    temp_round_1_game_4 = request.POST['game_4_pick'],
+    pick_set.round_1_game_1 = temp_round_1_game_1[0]
+    pick_set.round_1_game_2 = temp_round_1_game_2[0]
+    pick_set.round_1_game_3 = temp_round_1_game_3[0]
+    pick_set.round_1_game_4 = temp_round_1_game_4[0]
     pick_set.save()
     return redirect(f'/picks/{pick_set.id}/')
