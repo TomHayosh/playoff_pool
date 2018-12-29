c307f35927d91852f081feaf700f207d935f93c0
Tom Hayosh <thayosh@gmail.com>
2018-12-29 12:05:40 -0600

    FT works for updating picks now too

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.touch_actions import TouchActions
 from selenium.webdriver.common.keys import Keys
 import time
 
@@ -98,18 +97,16 @@ class NewVisitorTest(LiveServerTestCase):
 
         # He changes his mind and decides the visitors will win game 3
         inputbox3 = self.browser.find_element_by_id('game_3')
-        # touchactions = TouchActions(inputbox3)
-        # touchactions.doubleTap(inputbox3)
+        inputbox3.clear()
         inputbox3.send_keys('-3')
         inputbox3.send_keys(Keys.ENTER)
-        # time.sleep(3)
 
         # The page updates again, and now shows the new pick for game 3
         # along with the previous picks for the other games
-        # self.wait_for_entry_in_input_box('game_1', '10')
-        # self.wait_for_entry_in_input_box('game_2', '7')
-        # self.wait_for_entry_in_input_box('game_3', '-3')
-        # self.wait_for_entry_in_input_box('game_4', '-10')
+        self.wait_for_entry_in_input_box('game_1', '10')
+        self.wait_for_entry_in_input_box('game_2', '7')
+        self.wait_for_entry_in_input_box('game_3', '-3')
+        self.wait_for_entry_in_input_box('game_4', '-10')
 
     def test_user_can_submit_partial_pick_set(self):
         # Chuck knows what he wants for the first game
