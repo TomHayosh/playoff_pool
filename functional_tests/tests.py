f6429db261ee50b4c2c7b4319e8ecfd5b61ed35d
Tom Hayosh <thayosh@gmail.com>
2018-12-31 17:44:14 -0600

    Fixed FTs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,13 +16,26 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def wait_for_text_in_element(self, text, element_id):
+        start_time = time.time()
+        while True:
+            try:
+                element = self.browser.find_element_by_id(element_id)
+                self.assertIn(text, element.text)
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
     def wait_for_row_in_picks_table(self, row_text):
         start_time = time.time()
         while True:
             try:
-                table = self.browser.find_element_by_id('id_picks')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
+                ul = self.browser.find_element_by_id('picks_list')
+                lis = ul.find_elements_by_tag_name('li')
+                print(f'lis = {lis}')
+                self.assertIn(row_text, [row.text for row in lis])
                 return
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
@@ -72,7 +85,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # After years of running the playoff pool via email and Excel
         # spreadsheets, it is now managed from the cloud.
 
-        # Chuck goes to check out its homepage.
+        # Mike goes to check out its homepage.
         self.browser.get(self.live_server_url)
         # self.browser.get('http://tomhayosh.pythonanywhere.com/')
 
@@ -90,19 +103,20 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # He sees his name on the edit page
         self.wait_for_subheading('Mike Holton: Enter your picks')
 
-    @skip('fix')
     def test_can_enter_a_pick_set_and_view_it_later(self):
         # After years of running the playoff pool via email and Excel
         # spreadsheets, it is now managed from the cloud.
 
-        # Chuck goes to check out its homepage.
+        # Chuck creates a pick set
         self.browser.get(self.live_server_url)
         # self.browser.get('http://tomhayosh.pythonanywhere.com/')
 
-        # He notices the page title and header mention the NFL Playoff Pool.
-        self.assertIn('2019 NFL Playoff Pool', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('2019 NFL Playoff Pool', header_text)
+        inputbox = self.browser.find_element_by_id('participant')
+        inputbox.send_keys('Chuck Medhurst')
+        inputbox.send_keys(Keys.ENTER)
+
+        # He sees his name on the edit page
+        self.wait_for_subheading('Chuck Medhurst: Enter your picks')
 
         # He is able to create a new pick set for the first round of
         # four games.
@@ -115,31 +129,32 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox1.send_keys('10')
         inputbox2.send_keys('7')
         inputbox3.send_keys('3')
-        inputbox4.send_keys('-10')
+        inputbox4.send_keys('10')
+        # inputbox4.send_keys('-10')
 
         # When he hits enter, the page updates and shows the entered picks.
         inputbox4.send_keys(Keys.ENTER)
 
-        self.wait_for_row_in_picks_table('Game 1: 10')
-        self.wait_for_row_in_picks_table('Game 2: 7')
-        self.wait_for_row_in_picks_table('Game 3: 3')
-        self.wait_for_row_in_picks_table('Game 4: -10')
+        self.wait_for_text_in_element('Texans by 10', "pick_1")
+        self.wait_for_text_in_element('Cowboys by 7', "pick_2")
+        self.wait_for_text_in_element('Ravens by 3', "pick_3")
+        self.wait_for_text_in_element('Bears by 10', "pick_4")
 
-        # He changes his mind and decides the visitors will win game 3
+        # He changes his mind and decides the Bears will win by 56
         editbutton = self.browser.find_element_by_id('edit_picks')
         editbutton.send_keys(Keys.ENTER)
-        time.sleep(1)
-        inputbox3 = self.browser.find_element_by_id('game_3')
+        self.wait_for_subheading('Chuck Medhurst: Enter your picks')
+        inputbox3 = self.browser.find_element_by_id('game_4')
         inputbox3.clear()
-        inputbox3.send_keys('-3')
+        inputbox3.send_keys('56')
         inputbox3.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows the new pick for game 3
         # along with the previous picks for the other games
-        self.wait_for_row_in_picks_table('Game 1: 10')
-        self.wait_for_row_in_picks_table('Game 2: 7')
-        self.wait_for_row_in_picks_table('Game 3: -3')
-        self.wait_for_row_in_picks_table('Game 4: -10')
+        self.wait_for_text_in_element('Texans by 10', "pick_1")
+        self.wait_for_text_in_element('Cowboys by 7', "pick_2")
+        self.wait_for_text_in_element('Ravens by 3', "pick_3")
+        self.wait_for_text_in_element('Bears by 56', "pick_4")
 
     @skip('fix')
     def test_user_can_submit_partial_pick_set(self):
